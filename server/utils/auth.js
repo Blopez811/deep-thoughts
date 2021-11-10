5f41e58587d0d000f94ae1704bcc896ab2a0d3e5
Benicio Lopez <benicio.lopez@gmail.com>
2021-11-10 12:58:55 -0600

    set up mutations  and tested they all work

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -8,19 +8,20 @@ module.exports = {
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
+    // [ Seperate "Bearer" from "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token
         .split(' ')
         .pop()
         .trim();
     }
-
+    //if no token, return request object as is
     if (!token) {
       return req;
     }
 
     try {
+      // decode and attach user data to request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
